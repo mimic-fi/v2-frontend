6db82901670720f44d8027e50d9e5cba10a5c49b
brunitob <brunitob@gmail.com>
2022-11-14 13:09:01 -0300

    adding hooks to read smart-vaults / actions metadata

---
+++ src/hooks/useMetadata.js
@@ -0,0 +1,16 @@
+import { useQuery } from 'react-query'
+import axios from 'axios'
+
+const useMetadata = (type = null, chainId = 1, address = null) => {
+  return useQuery(['metadata', type, chainId, address], // data cached with unique id 
+  () => fetchMetadata(type, chainId, address))
+}
+
+const fetchMetadata = async (type, chainId, address) => {
+  // TODO: urlMetadata update domain address, split to enviroment file
+  const urlMetadata = `https://cdn.statically.io/gh/mimic-fi/v2-metadata/master/build/${type}/${chainId}/${address}`
+  const { data } = await axios.get(urlMetadata)
+  return data
+}
+
+export default useMetadata
