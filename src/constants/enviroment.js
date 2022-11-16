5a1c74e12a631e458605fce545063f90c9594c28
brunitob <brunitob@gmail.com>
2022-11-16 09:55:50 -0300

    add metadata to env

--- src/constants/enviroment.js
+++ src/constants/enviroment.js
@@ -9,7 +9,7 @@ export const isStaging = () => process.env.REACT_APP_VERCEL_ENV === 'preview'
 
 export const isProduction = () => process.env.REACT_APP_VERCEL_ENV === 'production'
 
-export const getChainId = () => process.env.REACT_APP_CHAIN_ID
+export const getChainId = () => process.env.REACT_APP_CHAIN_ID || 1
 
 export function getChainInfo() {
   const chainId = getChainId()
