6a52237358876af6b6f166005ac0c28a0e3a0a8d
brunitob <brunitob@gmail.com>
2022-11-16 09:15:39 -0300

    adding chainInfo and enviroment helper file

---
+++ src/constants/chainInfo.js
@@ -0,0 +1,24 @@
+import ethereumLogoUrl from 'assets/images/ethereum-logo.png'
+
+export const SupportedChainId = {
+    MAINNET: 1,
+    GOERLI: 5
+  }
+
+export const CHAIN_INFO = {
+    [SupportedChainId.MAINNET]: {
+      explorer: 'https://etherscan.io/',
+      label: 'Ethereum',
+      logoUrl: ethereumLogoUrl,
+      nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+      color: '#fff',
+    },
+    [SupportedChainId.GOERLI]: {
+      explorer: 'https://goerli.etherscan.io/',
+      label: 'Görli',
+      logoUrl: ethereumLogoUrl,
+      nativeCurrency: { name: 'Görli Ether', symbol: 'görETH', decimals: 18 },
+      color: '#bb437e',
+    },
+  }
+
---
+++ src/constants/enviroment.js
@@ -0,0 +1,20 @@
+import { CHAIN_INFO } from './chainInfo'
+
+export const CHAIN_THEGRAPH_URL = process.env.REACT_APP_THEGRAPH_URL
+
+// check if its vercel enviroment
+export const isDevelopment = () => !isStaging() && !isProduction()
+
+export const isStaging = () => process.env.REACT_APP_VERCEL_ENV === 'preview'
+
+export const isProduction = () => process.env.REACT_APP_VERCEL_ENV === 'production'
+
+export const getChainId = () => process.env.REACT_APP_CHAIN_ID
+
+export function getChainInfo() {
+  const chainId = getChainId()
+  if (chainId) {
+    return CHAIN_INFO[chainId] ?? undefined
+  }
+  return undefined
+}
