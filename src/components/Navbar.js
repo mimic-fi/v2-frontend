b96dec03bc2ea043f8bf12b69f6384a770d1f974
delfipolito <delfinapolito@gmail.com>
2022-11-17 15:56:10 -0300

    Move network info inside navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
+import NetworkInfo from './NetworkInfo'
 
 const Navbar = () => {
   return (
     <NavbarSection>
+      <NavbarContainer>
+        <NetworkInfo />
+      </NavbarContainer>
     </NavbarSection>
   )
 }
 
 const NavbarSection = styled.section`
   z-index: 100;
-  background: transparent;
-  background: #121418;
-  position: fixed;
-  top: 0;
+  background: #1f2021;
   width: 100%;
   margin: auto;
+`
+
+const NavbarContainer = styled.div`
+  margin: auto;
+  max-width: 1140px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
 `
 
 export default Navbar
