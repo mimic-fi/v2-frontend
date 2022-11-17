7109eb11ea31e7c818e3a931cffb9beb6db87b61
delfipolito <delfinapolito@gmail.com>
2022-11-17 15:49:09 -0300

    Show action icons

--- src/sections/Action.js
+++ src/sections/Action.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import moment from 'moment'
+import styled from 'styled-components'
 import TableRow from '../components/Table/TableRow'
 import TableCell from '../components/Table/TableCell'
 import { shortenAddress } from '../utils/web3-utils'
 import check from '../assets/success.svg'
+import defaultAction from '../assets/default-action.svg'
 import useActionMetadata from '../hooks/useActionMetadata'
 
 const Action = ({ primitives }) => {
   const item = primitives[0]
   const metadata = useActionMetadata(item.target)
-  console.log('metadata', metadata.data)
+  console.log('action', metadata)
   return (
     <TableRow key={item.id}>
       <TableCell>
@@ -17,14 +19,25 @@ const Action = ({ primitives }) => {
           ? moment.unix(item.executedAt).format('MMM Do, h:mm')
           : '-'}
       </TableCell>
-      <TableCell>{metadata.data ? metadata.data.title : item.type}</TableCell>
+      <TableCell>
+          <ActionIcon
+            src={metadata.data ? metadata.data.icon : defaultAction}
+            alt=""
+          />
+          {metadata.data ? metadata.data.title : item.type}
+      </TableCell>
       <TableCell>{metadata.data ? metadata.data.description : ''}</TableCell>
       <TableCell>{shortenAddress(item.sender)}</TableCell>
       <TableCell>
-        <img src={check} alt=""/>
+        <img src={check} alt="" />
       </TableCell>
     </TableRow>
   )
 }
+
+const ActionIcon = styled.img`
+  height: 23px;
+  margin-right: 15px;
+`
 
 export default Action
