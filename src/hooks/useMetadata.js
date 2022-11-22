b7106615f0528dbadf66889c83c97ed62e581190
brunitob <brunitob@gmail.com>
2022-11-22 12:56:51 -0300

    now using env vars

--- src/hooks/useMetadata.js
+++ src/hooks/useMetadata.js
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query'
 import axios from 'axios'
+import { METADATA_URL } from '../constants/enviroment'
 
 const useMetadata = (type = null, chainId = 1, address = null) => {
   return useQuery(
@@ -10,7 +11,7 @@ const useMetadata = (type = null, chainId = 1, address = null) => {
 
 const fetchMetadata = async (type, chainId, address) => {
   // TODO: urlMetadata update domain address, split to enviroment file
-  const urlMetadata = `https://cdn.statically.io/gh/mimic-fi/v2-metadata/master/build/${type}/${chainId}/${address}`
+  const urlMetadata = `${METADATA_URL}/${type}/${chainId}/${address}`
   const { data } = await axios.get(urlMetadata)
   return data
 }
