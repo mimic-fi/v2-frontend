48e0137571181e8d9e8d0262117058953d4c3020
brunitob <brunitob@gmail.com>
2022-11-24 10:36:56 -0300

    select component working

--- src/components/NetworkInfo.js
+++ src/components/NetworkInfo.js
@@ -1,33 +1,115 @@
 import styled from 'styled-components'
 import { getChainInfo } from '../constants/enviroment'
+import Select from 'react-select'
+import { useState } from 'react'
+import { CHAIN_INFO } from '../constants/chainInfo'
 
 
 const NetworkInfo = () => {
+  const [selectedOption, setSelectedOption] = useState(CHAIN_INFO[1])
   const chainInfo = getChainInfo()
+
+  const options = Object.keys(CHAIN_INFO).map(c => CHAIN_INFO[c])
+
+  const Control = props => {
+    const {
+      children,
+      innerRef,
+      innerProps
+    } = props
+    return (
+      <ControlContainer
+        ref={innerRef}
+        {...innerProps}
+      >
+        <ChainLogo src={selectedOption.logoUrl} />
+        {children}
+      </ControlContainer>
+    )
+  }
 
   return (
     <Container>
-      <NetwrokLogo src={chainInfo.logoUrl} />
-      <NetworkName>
-        {chainInfo.label}
-      </NetworkName>
+      <SelectElement
+        components={{ Option, Control }}
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={options}
+        classNamePrefix="react-select"
+      />
     </Container>
   )
 }
 
-const NetwrokLogo = styled.img`
- fill: aqua;
+const Option = props => {
+  const {
+    children,
+    isDisabled,
+    innerRef,
+    innerProps
+  } = props
+  return (
+    <OptionContainer
+      ref={innerRef}
+      {...innerProps}
+      isDisabled={isDisabled}
+    >
+      <ChainLogo src={CHAIN_INFO[props?.value].logoUrl} />
+      <ChainName>{children}</ChainName>
+    </OptionContainer>
+  )
+}
+
+const SelectElement = styled(Select)`
+  .react-select__control {
+    background-color: transparent;
+    border: none;
+    min-width: 170px;
+  }
+  .react-select__single-value {
+    color: white;
+  }
+  .react-select__menu {
+   background-color: #373839;
+   min-width: 170px;
+  }
+
+  .react-select__input-container{}
+  .react-select__indicators {}
+  .react-select__input {}
+  .react-select-container {}
 `
 
-const NetworkName = styled.div`
- padding: 0px 5px;
+const ChainLogo = styled.img`
+ width: 25px;
+ object-fit: scale-down;
+
+`
+const OptionContainer = styled.div`
+  display: flex;
+  margin: 0px 10px;
+  opacity: ${props => props.isDisabled ? '0.25' : '1'};
+
+`
+
+const ControlContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  opacity: ${props => props.isDisabled ? '0.25' : '1'};
+  text-align: end;
+`
+
+const ChainName = styled.div`
+ padding: 10px 10px;
+
 `
 
 const Container = styled.div`
   display: flex;
   height: 100%;
   align-items: center;
-  color: white
+  color: white;
 `
 
 export default NetworkInfo
