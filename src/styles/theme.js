11d734a81dfa5bb1e4803f15ff5016824be32ac0
brunitob <brunitob@gmail.com>
2022-12-12 22:03:05 -0300

    update color

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,7 @@
 const theme = {
   main: '#5542A9',
   mainDark: '#4347A3',
-  mainDefault: '#8286F2',
+  mainDefault: '#A996FF',
   backgroundDefault : '#252627',
   tableBorder: '#2B2D34',
   tableHeaderColor: '#fcfcfc'
