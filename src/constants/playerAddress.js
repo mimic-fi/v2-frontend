6ade8ff4ef889d556e2310539365cd4766b224f9
brunitob <brunitob@gmail.com>
2022-12-13 00:08:03 -0300

    lint

--- src/constants/playerAddress.js
+++ src/constants/playerAddress.js
@@ -2,8 +2,8 @@ import ethereumLogoUrl from '../assets/logo-navbar.svg'
 import theme from '../styles/theme'
 
 export const PLAYERS = {
-    // address + chainid
-    ['0xb3afb6db38a8e72905165c1fbb96772e63560790-1']: {
+     // eslint-disable-next-line
+    ['0xb3afb6db38a8e72905165c1fbb96772e63560790-1']: { // address + chainid
       name: 'Mimic Bot',
       logoUrl: ethereumLogoUrl,
       backgroundColor: theme.main
