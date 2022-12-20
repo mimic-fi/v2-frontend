6267f20c8448747f763da425ea7991772b7328e5
delfipolito <delfinapolito@gmail.com>
2022-12-20 17:43:35 -0300

    Page not found restyling & Smart vault not found

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled from 'styled-components'
-import search from '../assets/search.svg'
+import ChainSelector from './ChainSelector'
 
-function SearchBar() {
-  const [searchInput, setSearchInput] = useState('')
+function SearchBar({ value = '' }) {
+  const [searchInput, setSearchInput] = useState(value)
   const navigate = useNavigate()
 
   const handleChange = e => {
@@ -26,8 +26,8 @@ function SearchBar() {
         value={searchInput}
         required
       />
-      <button type="submit">
-        <img src={search} alt="" />
+      <button>
+        <ChainSelector />
       </button>
     </Search>
   )
@@ -35,10 +35,9 @@ function SearchBar() {
 
 const Search = styled.form`
   position: relative;
-  width: 30rem;
-  background: #a996ff;
+  width: 630px;
   border-radius: 4px;
-  margin: 30px auto;
+  margin: 30px 0;
   font-family: 'GTWalsheimPro';
   font-style: normal;
   font-weight: 400;
@@ -57,28 +56,33 @@ const Search = styled.form`
   input {
     outline: 0;
     width: 100%;
-    background: #fff;
+    background: #2b2d34;
     padding: 0 1.6rem;
-    border-radius: 4px;
+    border-radius: 40px;
     appearance: none;
     transition: all 0.5s cubic-bezier(0, 0, 0.43, 1.49);
     transition-property: width, border-radius;
     z-index: 1;
     position: relative;
+    color: white;
+    ::placeholder {
+      color: white;
+      opacity: 1;
+    }
   }
   button {
     display: none;
     position: absolute;
     top: 0;
     right: 0;
-    width: 70px;
+    width: 90px;
     font-weight: bold;
-    background: #a996ff;
-    border-radius: 0 4px 4px 0;
+    background: #2b2d34;
+    border-radius: 0 40px 40px 0;
   }
   input:not(:placeholder-shown) {
-    border-radius: 4px 0 0 4px;
-    width: calc(100% - 70px);
+    border-radius: 40px 0 0 40px;
+    width: calc(100% - 90px);
     + button {
       display: block;
     }
