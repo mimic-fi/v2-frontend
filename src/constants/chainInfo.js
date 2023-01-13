351014e6f2ef2b466f10cfbc907c6ff96a6f71ad
Bruno Balzani <brunitob@users.noreply.github.com>
2023-01-12 23:45:47 -0300

    add support for gnosis polygon optimism arbitrum (#27)

--- src/constants/chainInfo.js
+++ src/constants/chainInfo.js
@@ -4,6 +4,7 @@ import optimismLogoUrl from '../assets/chainLogos/optimism-logo.svg'
 import polygonLogoUrl from '../assets/chainLogos/polygon-logo.svg'
 import gnosisLogoUrl from '../assets/chainLogos/gnosis-logo.svg'
 import arbitrumLogoUrl from '../assets/chainLogos/arbitrum-logo.svg'
+import zksyncLogoUrl from '../assets/chainLogos/zksync.svg'
 
 export const SupportedChainId = {
   MAINNET: 1,
@@ -12,7 +13,8 @@ export const SupportedChainId = {
   POLYGON: 137,
   GNOSIS: 100,
   ARBITRUM: 42161,
-  MUMBAI: 80001
+  MUMBAI: 80001,
+  ZKSYNC: 324
 }
 
 export const DEFAULT_CHAIN_ID = SupportedChainId.MAINNET
@@ -26,14 +28,7 @@ export const CHAIN_INFO = {
     color: '#fff',
     value: SupportedChainId.MAINNET,
   },
-  [SupportedChainId.GOERLI]: {
-    explorer: 'https://goerli.etherscan.io/',
-    label: 'Görli',
-    logoUrl: goerliLogoUrl,
-    nativeCurrency: { name: 'Görli Ether', symbol: 'görETH', decimals: 18 },
-    color: '#bb437e',
-    value: SupportedChainId.GOERLI,
-  },
+
   [SupportedChainId.MUMBAI]: {
     explorer: 'https://mumbai.polygonscan.com/',
     label: 'Mumbai',
@@ -41,42 +36,57 @@ export const CHAIN_INFO = {
     nativeCurrency: { name: 'Matic', symbol: 'MATIC', decimals: 18 },
     color: '#de4437',
     value: SupportedChainId.MUMBAI,
+    isTestnet: true
   },
   [SupportedChainId.OPTIMISM]: {
-    explorer: '',
+    explorer: 'https://optimistic.etherscan.io/',
     label: 'Optimism',
     logoUrl: optimismLogoUrl,
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     color: '#bb437e',
     value: SupportedChainId.OPTIMISM,
-    isDisabled: true,
   },
   [SupportedChainId.POLYGON]: {
-    explorer: '',
+    explorer: 'https://polygonscan.com/',
     label: 'Polygon',
     logoUrl: polygonLogoUrl,
     nativeCurrency: { name: 'Görli Ether', symbol: 'görETH', decimals: 18 },
     color: '#bb437e',
     value: SupportedChainId.POLYGON,
-    isDisabled: true,
   },
   [SupportedChainId.GNOSIS]: {
-    explorer: '',
+    explorer: 'https://gnosisscan.io/',
     label: 'Gnosis',
     logoUrl: gnosisLogoUrl,
     nativeCurrency: { name: 'xDAI', symbol: 'xDAI', decimals: 18 },
     color: '#bb437e',
     value: SupportedChainId.GNOSIS,
-    isDisabled: true,
   },
   [SupportedChainId.ARBITRUM]: {
-    explorer: '',
+    explorer: 'https://arbiscan.io/',
     label: 'Arbitrum',
     logoUrl: arbitrumLogoUrl,
     nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
     color: '#bb437e',
     value: SupportedChainId.ARBITRUM,
-    isDisabled: true,
+  },
+  [SupportedChainId.ZKSYNC]: {
+    explorer: 'https://explorer.zksync.io/',
+    label: 'zkSync',
+    logoUrl: zksyncLogoUrl,
+    nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+    color: '#bb437e',
+    value: SupportedChainId.ZKSYNC,
+    isDisabled: true
+  },
+  [SupportedChainId.GOERLI]: {
+    explorer: 'https://goerli.etherscan.io/',
+    label: 'Görli',
+    logoUrl: goerliLogoUrl,
+    nativeCurrency: { name: 'Görli Ether', symbol: 'görETH', decimals: 18 },
+    color: '#bb437e',
+    value: SupportedChainId.GOERLI,
+    isTestnet: true
   },
 }
 
@@ -87,4 +97,12 @@ export const CHAIN_SUBGRAPH_URL = {
     'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-goerli',
   [SupportedChainId.MUMBAI]:
     'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-mumbai',
+  [SupportedChainId.GNOSIS]:
+    'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-gnosis',
+  [SupportedChainId.POLYGON]:
+    'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-polygon',
+  [SupportedChainId.OPTIMISM]:
+    'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-optimism',
+  [SupportedChainId.ARBITRUM]:
+    'https://api.thegraph.com/subgraphs/name/mimic-fi/v2-arbitrum',
 }
