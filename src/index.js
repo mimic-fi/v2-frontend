27102abc06cc47bf3ffbaf83fd7a10ee22d00e21
delfipolito <delfinapolito@gmail.com>
2023-02-03 15:19:52 -0300

    Remove hash from urls (#37)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import { AppProvider } from './context/appContext'
 import './index.css'
@@ -8,10 +8,10 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
