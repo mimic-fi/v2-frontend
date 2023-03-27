662bd9abdd7396a788f22c5153f74ab2afd98676
delfipolito <delfinapolito@gmail.com>
2023-03-27 11:21:30 -0300

    Deprecated feature on list page (#47)

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
-import { Link } from 'react-router-dom'
 import Page from '../components/Page'
+import { Skeleton } from '../styles/general'
 import { Container, Hl } from '../styles/texts'
 import SmartVaultCard from '../sections/SmartVaultCard'
 import useSmartVaults from '../hooks/useSmartVaults'
@@ -9,22 +9,37 @@ import useSmartVaults from '../hooks/useSmartVaults'
 const SmartVaults = () => {
   //todo: add loader
   const { data, isLoading } = useSmartVaults()
+  if (isLoading) {
+    return (
+      <Page sidebar={false}>
+        <SmartVaultSection>
+          <Title>Browse Smart vaults</Title>
+          <ListContainer>
+            <br />
+            <Skeleton height="890px" width="100%" marginBottom="30px" />
+          </ListContainer>
+        </SmartVaultSection>
+      </Page>
+    )
+  }
 
   return (
     <Page sidebar={false}>
       <SmartVaultSection>
         <Title>Browse Smart vaults</Title>
         <ListContainer>
-          {isLoading ? 'Loading Smart Vaults...' :
-            data?.length > 0 ? data.map(item => {
-              return (
-                  <Link key={item.id} to={'/smart-vaults/' + item.id}>
-                    <SmartVaultCard smartVault={item} />
-                  </Link>
-              )
-            })
-              : 'No Smart Vaults here'
-          }
+          {data?.length > 0 ? (
+            <>
+              {data.map(item => {
+                return <SmartVaultCard smartVault={item} active={true} key={'active' + item.id}/>
+              })}
+              {data.map(item => {
+                return <SmartVaultCard smartVault={item} active={false} key={'noactive' + item.id} />
+              })}
+            </>
+          ) : (
+            'No Smart Vaults here'
+          )}
         </ListContainer>
       </SmartVaultSection>
     </Page>
@@ -49,10 +64,11 @@ const ListContainer = styled(Container)`
   justify-content: center;
   flex-wrap: wrap;
   gap: 25px;
+  max-width: 1180px;
 `
 
 const Title = styled(Hl)`
-  margin: 100px auto;
+  margin: 0 auto 100px auto;
 `
 
 export default SmartVaults
